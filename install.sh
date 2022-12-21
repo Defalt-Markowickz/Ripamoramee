245586ed3b26e6cf68166e02df4f02d463502af9
Defalt00FFFF <85587928+Defalt00FFFF@users.noreply.github.com>
2022-12-21 22:29:48 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+echo "Instalando requisitos"
+sleep 2
+
+pip install base64
+pip install colorama
+pip install time
+pip install pycryptodome
+
+echo "Requisitos instalados correctamente, saliendo del script... :D"
+clear
+
